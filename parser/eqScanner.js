f294d7854cfa52951b2c7437448ffb1f2302b15a
Ben McCormick <ben.mccormick@alumni.duke.edu>
2012-10-28 20:27:02 -0400

    Added the scanner.  Tokenizer and scanner now functional.

--- parser/eqScanner.js
+++ parser/eqScanner.js
@@ -7,40 +7,111 @@
 
 var EQScanner = (function() {
     var EQS ={};
-    
-    EQS.initialize = function(tokens){
+    var tokens = [];
+    var sym = [];
+    var currenttok = "x";
+    var currentref = null;
+    var vars = {};
 
-        return ;
+    // For now these are going to be in code.  Should be moved 
+    // To a props file at some point
+    var funcs, ops, bifuncs, puncs, unops;
+
+    funcs = ["sqr(","sqrt(","log(","ln(","exp(","floor(","ceil(","neg(","rnd(",
+            "sin(","cos(","tan(","asin(","acos(","atan(","abs(","("];
+    ops = ["+","-","*","/","%","^","*","|","&"];
+    bifuncs = ["min(","max(","perm(","comb("];
+    puncs = [",",")","#"];
+    unops = ["!"];
+
+    var setReference = function(tok)
+    {
+        currentref = isInSym((tok+"").toLowerCase());
+        var value = (currenttok === "d") ? BigDecimal(tok) : BigDecimal("0");
+        if(currentref === null)
+        {
+            currentref = sym.length;
+            sym.push({
+                symbol:currenttok,
+                text: (tok+"").toLowerCase(),
+                value: value
+            });
+        }
     };
 
-    EQS.newExpression = function(tokens){
-
-        return ;
+    var isInSym = function(tok)
+    {
+        /*Return the reference if the token is in the symbol table.
+        returns null otherwise*/
+        for (var i = sym.length - 1; i >= 0; i--) {
+            if(sym[i].text === tok)
+            {
+                return i;
+            }
+        }
+        return null;
     };
 
-    EQS.nextToken = function(){
+    EQS.newExpression = function(tokenarr){
 
-        return ;
+        tokens = tokenarr.reverse();
     };
+
+    EQS.scanNext = function(tokenarr){
+        var tok = nextToken();
+        if(!tok)
+        {
+            return null;
+        }
+        var numx = /^[\-+]?[0-9]*\.?[0-9]+$/;
+        var varx = /^[A-Za-z]+[0-9]*$/;
+        //Find the correct category for the current token
+        currenttok =($.inArray(tok, funcs) !== -1)? "f" :
+                    ($.inArray(tok,bifuncs) !== -1) ? "n" :
+                    ($.inArray(tok,ops) !== -1) ? "b" :
+                    ($.inArray(tok,unops) !== -1) ? "u" :
+                    (numx.exec(tok)) ? "d" :
+                    ($.inArray(tok,puncs) !== -1) ? tok :
+                    (varx.exec(tok)) ? "v" : errorHandle(tok);
+
+        if(currenttok.indexOf("Error:") === -1)
+        {
+            setReference(tok);
+        }
+        return currenttok;
+
+    };
+
+    var nextToken = function(){
+        //if there are tokens, remove and return the next one
+        return (tokens.length) ? tokens.pop() : null;   
+    };
+
+    var errorHandle = function(tok){
+        return "Error: Invalid token - " +tok;
+    }
 
     EQS.hasTokens = function(){
 
-        return ;
+        return !tokens.length;
     };
 
-    EQS.getRef = function(index){
+    EQS.getRef = function(){
 
-        return ;
+        return currentref;
+    };
+
+    EQS.getRefData = function(index){
+        return sym[index];
     };
 
     EQS.getVar = function(varname){
 
-        return ;
+        return vars[varname];
     };
 
     EQS.setVar = function(varname, val){
-
-        return ;
+        vars[varname] = val;
     };
     return EQS;
 }());
