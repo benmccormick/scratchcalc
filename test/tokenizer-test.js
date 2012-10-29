ffb7366560ba75aff32bf6261e1b824736e13793
Ben McCormick <ben.mccormick@alumni.duke.edu>
2012-10-28 23:07:03 -0400

    Added qUnit as a test framework.

---
+++ test/tokenizer-test.js
@@ -0,0 +1,44 @@
+/*global ok:false EQTokenizer:false test:false equal:false
+    deepEqual:false*/
+
+test( "Tokenizer Exists", function() {
+    ok( (EQTokenizer), "Tokenizer Exists" );
+});
+
+test( "Basic Tokenize", function() {
+    EQTokenizer.tokenize("1 + 1");
+    var list = EQTokenizer.getList();
+    deepEqual( list,["1","+","1"], "1+1 Test" );
+
+    EQTokenizer.tokenize("2 - 2");
+    list = EQTokenizer.getList();
+    deepEqual( list,["2","-","2"], "2-2 Test" );
+
+    EQTokenizer.tokenize("3 * 4");
+    list = EQTokenizer.getList();
+    deepEqual( list,["3","*","4"], "3*4 Test" );
+
+
+    EQTokenizer.tokenize("10 / 2");
+    list = EQTokenizer.getList();
+    deepEqual( list,["10","/","2"], "10/2 Test" );
+});
+
+test( "Negative Numbers", function() {
+    EQTokenizer.tokenize("-1 + 2");
+    var list = EQTokenizer.getList();
+    deepEqual( list,["-1","+","2"], "-1 + 2 Test" );
+
+    EQTokenizer.tokenize("2 - 2");
+    list = EQTokenizer.getList();
+    deepEqual( list,["2","-","2"], "2-2 Test" );
+
+    EQTokenizer.tokenize("3 * -4");
+    list = EQTokenizer.getList();
+    deepEqual( list,["3","*","-4"], "3* -4 Test" );
+
+
+    EQTokenizer.tokenize("(10) - 2");
+    list = EQTokenizer.getList();
+    deepEqual( list,["(","10", ")","-","2"], "(10) - 2 Test" );
+});
