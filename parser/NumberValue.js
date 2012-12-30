f9534a4bac99f73ba0570aa07678f9afa0eff67a
Ben McCormick <ben.mccormick@alumni.duke.edu>
2012-12-30 14:39:21 -0500

    Fixed Decimal Precision!!! Makes me happy anyway :)

--- parser/NumberValue.js
+++ parser/NumberValue.js
@@ -2,8 +2,9 @@
  /*global BigDecimal:false RoundingMode:false EQParser:false*/
 
 
-var NumberValue = function(value,units){
+var NumberValue = function(value,units,divisor){
     this.num = new BigDecimal(value);
+    this.divisor = divisor || new BigDecimal("1");
     this.units = units;
 };
 
@@ -19,44 +20,46 @@ NumberValue.prototype.add = function(othernumber){
     var nums = convertUnits(this,othernumber);
     var sum = nums[0].num.add(nums[1].num);
     return new NumberValue(sum.toString(), 
-        (nums[0].units || nums[1].units));
+        (nums[0].units || nums[1].units),nums[0].divisor);
 };
 
 NumberValue.prototype.subtract = function(othernumber){
     var nums = convertUnits(this,othernumber);
     var difference = nums[0].num.subtract(nums[1].num);
     return new NumberValue(difference.toString(), 
-        (nums[0].units || nums[1].units));
+        (nums[0].units || nums[1].units),nums[0].divisor);
 };
 
 NumberValue.prototype.multiply = function(othernumber){
-    var product = this.num.multiply(othernumber.num);
-    return new NumberValue(product.toString(),
-        (this.units || othernumber.units)
-        );
+    var numerator = this.num.multiply(othernumber.num);
+    var denominator = this.divisor.multiply(othernumber.divisor);
+    return new NumberValue(numerator.toString(),
+        (this.units || othernumber.units),denominator);
 };
 
 NumberValue.prototype.divide = function(othernumber){
-    var quotient = this.num.divide(othernumber.num, 20,RoundingMode.HALF_UP());
-    return new NumberValue(quotient.toString(),
-        (this.units||othernumber.units)
-        );
+    var numerator = this.num.multiply(othernumber.divisor);
+    var denominator = this.divisor.multiply(othernumber.num);
+    return new NumberValue(numerator.toString(),
+        (this.units||othernumber.units),denominator);
 };
 
 NumberValue.prototype.compareTo = function(othernumber){
     var nums = convertUnits(this,othernumber);
-    return nums[0].num.compareTo(nums[1].num);
+
+    return getDecimalVal(nums[0]).compareTo(getDecimalVal(nums[1]));
 };
 
 NumberValue.prototype.toString = function(){
     var unitstr = (this.units) ? " "+this.units : "";
-    return trimZeros(this.num.toString()) + unitstr;
+    return trimZeros(getDecimalVal(this).toString()) + unitstr;
 };
 
 /**
 * Converts units for when units need to be synced up
 * If the units are the same type, but different value, it will work to convert 
 * them into the first unit
+* Also evens out the divisors
 **/
 function convertUnits(num1,num2){
     var info1 = EQParser.getUnitInfo(num1.units);
@@ -65,16 +68,21 @@ function convertUnits(num1,num2){
         (num1.units === num2.units) ||(info1.type !== info2.type)){
         //if one of the units doesn't exist, they're the same units,
         //or type is not the same, move on
-        return [num1,num2];
     }
     else{
         //convert num2 to num1 units
         var multiple = new BigDecimal((info1.multiple/info2.multiple)+"");
         num2.num = (num2.num.divide(multiple, 20, RoundingMode.HALF_DOWN()));
         num2.units = num1.units;
-        return [num1, num2];
     }
-
+    if(num1.divisor.compareTo(num2.divisor) !== 0){
+        var div1 = num1.divisor;
+        num1.divisor = num1.divisor.multiply(num2.divisor);
+        num1.num = num1.num.multiply(num2.divisor);
+        num2.divisor = num1.divisor;
+        num2.num = num2.num.multiply(div1);
+    }
+    return [num1, num2];
 };
 
 function trimZeros(numString){
@@ -83,3 +91,6 @@ function trimZeros(numString){
     return numString;             
 };
 
+function getDecimalVal(number){
+    return number.num.divide(number.divisor,20, RoundingMode.HALF_DOWN());
+}
