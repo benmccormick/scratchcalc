0754ed4e4a5cd7f53181976e88757d611f998a2b
Ben McCormick <ben.mccormick@alumni.duke.edu>
2013-03-20 21:35:42 -0400

    Added some tests and added minifying to the ant build file.

--- deletethis.js
+++ deletethis.js
@@ -12,7 +12,7 @@
       comparison = result.compareTo(number);
       return expect(comparison).toEqual(0);
     });
-    return describe("specifically it ", function() {
+    return describe("specifically ", function() {
       it("can add numbers", function() {
         var comparison, number, result;
         result = EQParser.parse("1+1");
@@ -78,7 +78,7 @@
         comparison = result.compareTo(number);
         return expect(comparison).toEqual(0);
       });
-      return it("can divide numbers", function() {
+      it("can divide numbers", function() {
         var comparison, number, result;
         result = EQParser.parse("10/2");
         number = new NumberValue("5");
@@ -101,6 +101,76 @@
         comparison = result.compareTo(number);
         return expect(comparison).toEqual(0);
       });
+      return it("can handle factorials", function() {
+        var comparison, number, result;
+        result = EQParser.parse("3!");
+        number = new NumberValue("6");
+        comparison = result.compareTo(number);
+        console.log("sanity", number.toString(), result.toString());
+        expect(comparison).toEqual(0);
+        result = EQParser.parse("1!");
+        number = new NumberValue("1");
+        comparison = result.compareTo(number);
+        expect(comparison).toEqual(0);
+        result = EQParser.parse("1!");
+        number = new NumberValue("1");
+        comparison = result.compareTo(number);
+        expect(comparison).toEqual(0);
+        result = EQParser.parse("(6-2)!");
+        number = new NumberValue("24");
+        comparison = result.compareTo(number);
+        return expect(comparison).toEqual(0);
+      });
+    });
+  });
+
+  describe("A Tokenizer", function() {
+    var getVar;
+    getVar = function(variable) {
+      if (variable === "x") {
+        return 5;
+      } else {
+        return null;
+      }
+    };
+    it("exists in the calculator", function() {
+      return expect(EQTokenizer).toBeDefined();
+    });
+    it("can split a statement into its component parts", function() {
+      var list;
+      EQTokenizer.tokenize("1 + 1", getVar);
+      list = EQTokenizer.getList();
+      expect(list).toEqual(["1", "+", "1"]);
+      EQTokenizer.tokenize("2 - 2", getVar);
+      list = EQTokenizer.getList();
+      expect(list).toEqual(["2", "-", "2"]);
+      EQTokenizer.tokenize("3 * 4", getVar);
+      list = EQTokenizer.getList();
+      expect(list).toEqual(["3", "*", "4"]);
+      EQTokenizer.tokenize("10 / 2", getVar);
+      list = EQTokenizer.getList();
+      expect(list).toEqual(["10", "/", "2"]);
+      EQTokenizer.tokenize("sin(x)", getVar);
+      list = EQTokenizer.getList();
+      return expect(list).toEqual(["sin(", "x", ")"]);
+    });
+    return it("can handle negative numbers", function() {
+      var list;
+      EQTokenizer.tokenize("-1 + 2", getVar);
+      list = EQTokenizer.getList();
+      expect(list).toEqual(["-1", "+", "2"]);
+      EQTokenizer.tokenize("2 - 2", getVar);
+      list = EQTokenizer.getList();
+      expect(list).toEqual(["2", "-", "2"]);
+      EQTokenizer.tokenize("3 * -4", getVar);
+      list = EQTokenizer.getList();
+      expect(list).toEqual(["3", "*", "-4"]);
+      EQTokenizer.tokenize("300+7-2", getVar);
+      list = EQTokenizer.getList();
+      expect(list).toEqual(["300", "+", "7", "-", "2"]);
+      EQTokenizer.tokenize("(10) - 2", getVar);
+      list = EQTokenizer.getList();
+      return expect(list).toEqual(["(", "10", ")", "-", "2"]);
     });
   });
 
